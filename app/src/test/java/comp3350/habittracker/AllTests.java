58f0794e20a98c75e5b8dc3334c7682c9306c94d
sohalg <sohalg@myumanitoba.ca>
2020-02-04 22:56:45 -0600

    tests for HabitDateValidator

--- app/src/test/java/comp3350/habittracker/AllTests.java
+++ app/src/test/java/comp3350/habittracker/AllTests.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp3350.habittracker.Logic.CalendarDateValidatorTest;
+import comp3350.habittracker.Logic.HabitDateValidatorTest;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        CalendarDateValidatorTest.class
+        CalendarDateValidatorTest.class,
+        HabitDateValidatorTest.class
 })
 public class AllTests {
 }
